a9478468216825238b2bcb1ed6e67975971d713b
unknown <sumanzzz2024@gmail.com>
2025-05-12 17:12:25 +0530

    hello

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const students = ["Ankan Mahato","Bidisha Sarkar","Itu Mondal", "Mayuri Sarkar","Somnath Mondal","Shruti Pandey"];
 const container = document.getElementById("content");
-students.forEach((e)=>{
-  // if(!localStorage.getItem(e)){
-    // localStorage.setItem(e,false)
-  // }
-})
+const time = new Date();
+const month = time.getMonth();
+const date = time.getDate();
+const hours = time.getHours();
+const minutes = time.getMinutes();
+window.onload = function(){
   students.forEach(e=>{
-    const isChecked = localStorage.getItem(e)==='true'
-    
+    let isChecked = localStorage.getItem(e)==='true'
     console.log(e)
     container.innerHTML += `<div class="box"><h2>${e}</h2> <input type="checkbox" name="${e}" id="${e.replace(" ","")}" class="names" ${isChecked ? 'checked':''}></div>`
+    if(date === 1 && hours === 0 && minutes === 0){
+      localStorage.setItem(e,false)
+    }
   })
+}
+  
   const checkboxes  = document.getElementsByClassName('names');
   Array.from(checkboxes).forEach((checkbox)=>{
     checkbox.addEventListener('change',(e)=>{
@@ -19,5 +24,8 @@ students.forEach((e)=>{
       localStorage.setItem(studentName,String(isChecked))
     })
   })
+  const monthsArray = ['January','Feburary','March','April','May','June','July','August','September','October','November','December']
+  const mth = document.getElementById('month')
+  mth.innerText += `Month of ${monthsArray[month]}`  
   
   
